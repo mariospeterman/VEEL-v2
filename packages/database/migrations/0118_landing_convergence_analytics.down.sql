e018ea7b1322bca879f3996c697fc4976914f8ac
mariospeterman <mariospeterman@gmail.com>
2026-08-25 05:15:39 +0200

    fix(db): validate landing constraints with reduced locking

--- packages/database/migrations/0118_landing_convergence_analytics.down.sql
+++ packages/database/migrations/0118_landing_convergence_analytics.down.sql
@@ -11,25 +11,39 @@ where event_key in (
 );
 
 alter table analytics_onboarding_daily
-  drop constraint if exists analytics_onboarding_daily_event_key_check;
+  add constraint analytics_onboarding_daily_event_key_check_rollback check (event_key in (
+    'landing_viewed', 'login_opened', 'onboarding_opened', 'auth_method_selected',
+    'wallet_runtime_ready', 'wallet_authentication_completed', 'wallet_ownership_verified',
+    'profile_step_viewed', 'profile_step_completed', 'age_step_started',
+    'age_step_completed', 'age_step_failed', 'protected_app_entered',
+    'onboarding_abandoned', 'returning_login_completed', 'account_not_found'
+  )) not valid;
 
 alter table analytics_onboarding_daily
-  add constraint analytics_onboarding_daily_event_key_check check (event_key in (
+  validate constraint analytics_onboarding_daily_event_key_check_rollback;
+
+alter table analytics_onboarding_daily
+  drop constraint analytics_onboarding_daily_event_key_check;
+
+alter table analytics_onboarding_daily
+  rename constraint analytics_onboarding_daily_event_key_check_rollback
+  to analytics_onboarding_daily_event_key_check;
+
+alter table analytics_onboarding_journey_events
+  add constraint analytics_onboarding_journey_events_event_key_check_rollback check (event_key in (
     'landing_viewed', 'login_opened', 'onboarding_opened', 'auth_method_selected',
     'wallet_runtime_ready', 'wallet_authentication_completed', 'wallet_ownership_verified',
     'profile_step_viewed', 'profile_step_completed', 'age_step_started',
     'age_step_completed', 'age_step_failed', 'protected_app_entered',
     'onboarding_abandoned', 'returning_login_completed', 'account_not_found'
-  ));
+  )) not valid;
 
 alter table analytics_onboarding_journey_events
-  drop constraint if exists analytics_onboarding_journey_events_event_key_check;
+  validate constraint analytics_onboarding_journey_events_event_key_check_rollback;
 
 alter table analytics_onboarding_journey_events
-  add constraint analytics_onboarding_journey_events_event_key_check check (event_key in (
-    'landing_viewed', 'login_opened', 'onboarding_opened', 'auth_method_selected',
-    'wallet_runtime_ready', 'wallet_authentication_completed', 'wallet_ownership_verified',
-    'profile_step_viewed', 'profile_step_completed', 'age_step_started',
-    'age_step_completed', 'age_step_failed', 'protected_app_entered',
-    'onboarding_abandoned', 'returning_login_completed', 'account_not_found'
-  ));
+  drop constraint analytics_onboarding_journey_events_event_key_check;
+
+alter table analytics_onboarding_journey_events
+  rename constraint analytics_onboarding_journey_events_event_key_check_rollback
+  to analytics_onboarding_journey_events_event_key_check;
